a10b8df09edb4ab5b10db3ad55aedd0ad320c471
Alexander Levashov <alevash1@gmail.com>
2023-01-09 17:00:18 +0300

    Fix get_utm_data (removed memoize)

--- lib/utm_tracker/helper.rb
+++ lib/utm_tracker/helper.rb
@@ -5,7 +5,7 @@ module UtmTracker
     protected
 
     def get_utm_data
-      session[:utm_data] ||= {
+      session[:utm_data] = {
         source: params[:utm_source],
         medium: params[:utm_medium],
         content: params[:utm_content],
