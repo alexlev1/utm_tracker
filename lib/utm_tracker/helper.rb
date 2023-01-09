38879c8bc13668ca456c3417beac9c4f3c010626
Alexander Levashov <alevash1@gmail.com>
2023-01-09 18:16:46 +0300

    Fix UtmTracker::Helper

--- lib/utm_tracker/helper.rb
+++ lib/utm_tracker/helper.rb
@@ -2,16 +2,36 @@
 
 module UtmTracker
   module Helper
+    UTM_TAGS = [
+      "source",
+      "campaign",
+      "content",
+      "medium",
+      "term"
+    ].freeze
+
     protected
 
     def get_utm_data
-      session[:utm_data] = {
-        source: params[:utm_source],
-        medium: params[:utm_medium],
-        content: params[:utm_content],
-        campaign: params[:utm_campaign],
-        term: params[:utm_term],
-      }
+      session[:utm_data] = if utm_data_present?
+        {
+          source: params[:utm_source],
+          campaign: params[:utm_campaign],
+          content: params[:utm_content],
+          medium: params[:utm_medium],
+          term: params[:utm_term],
+        }
+      else
+        {}
+      end
+    end
+
+    def utm_data_present?
+      UTM_TAGS.each do |tag|
+        return true if params["utm_#{tag}".to_sym].present?
+      end
+
+      false
     end
   end
 end
